a82a0c0ff5a9760ed23de619149721d17cc4a2e6
Robson Carolino <robsoncarolino96@gmail.com>
2024-11-18 08:33:15 -0300

    Primeira atualização

---
+++ JS.js
@@ -0,0 +1,22 @@
+function atualizaData() {
+    const dataInicio = new Date('2023-12-24T14:20:00');
+    const agora = new Date();
+
+    const diferencaTempo = agora - dataInicio;
+
+    const anos = Math.floor(diferencaTempo / (1000 * 60 * 60 * 24 * 365.25));
+    const meses = Math.floor(diferencaTempo / (1000 * 60 * 60 * 24 * 30.44));
+    const semanas = Math.floor(diferencaTempo / (1000 * 60 * 60 * 24 * 7));
+    const dias = Math.floor(diferencaTempo / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((diferencaTempo % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutos = Math.floor((diferencaTempo % (1000 * 60 * 60)) / (1000 * 60));
+    const segundos = Math.floor((diferencaTempo % (1000 * 60)) / 1000);
+
+    document.getElementById('data').innerText = `${anos} anos, ${meses} meses, ${semanas} semanas, ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos`;
+}
+
+// Atualiza a cada segundo (1000 milissegundos)
+setInterval(atualizaData, 1000);
+
+// Chama a função imediatamente para exibir a idade ao carregar a página
+atualizaData();
